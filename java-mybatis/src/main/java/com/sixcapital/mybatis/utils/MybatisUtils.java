41352cc5ee6ce2d855fb3434e4b36bc2c31dfc57
moweiquan <295597613@qq.com>
2020-09-02 15:28:02 +0800

    mybatis-注解CRUD

--- java-mybatis/src/main/java/com/sixcapital/mybatis/utils/MybatisUtils.java
+++ java-mybatis/src/main/java/com/sixcapital/mybatis/utils/MybatisUtils.java
@@ -28,7 +28,7 @@ public class MybatisUtils {
     //既然有了 SqlSessionFactory，顾名思义，我们就可以从中获得 SqlSession 的实例了。
     // SqlSession 完全包含了面向数据库执行 SQL 命令所需的所有方法
     public static SqlSession getSqlSession() {
-        return sqlSessionFactory.openSession();
+        return sqlSessionFactory.openSession(true);
     }
 
 }
--- java-mybatis/src/test/java/com/sixcapital/mybatis/mapper/DepartmentMapperTest.java
+++ java-mybatis/src/test/java/com/sixcapital/mybatis/mapper/DepartmentMapperTest.java
@@ -25,4 +25,63 @@ public class DepartmentMapperTest {
         sqlSession.close();
 
     }
+
+    @Test
+    public void getDepartmentByIdTest() {
+        //获取sqlSession对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        //获取Mapper对象
+        DepartmentMapper mapper = sqlSession.getMapper(DepartmentMapper.class);
+
+        Department department = mapper.getDepartmentById(20);
+        System.out.println(department);
+        sqlSession.close();
+    }
+
+    @Test
+    public void addDepartmentTest() {
+        //获取sqlSession对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        //获取Mapper对象
+        DepartmentMapper mapper = sqlSession.getMapper(DepartmentMapper.class);
+
+        Department department = new Department(60, "行政部", "南京");
+
+        int result = mapper.addDepartment(department);
+        if (result > 0) {
+            System.out.println("添加部门操作成功");
+        }
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void updateDepartmentTest() {
+        //获取sqlSession对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        //获取Mapper对象
+        DepartmentMapper mapper = sqlSession.getMapper(DepartmentMapper.class);
+
+        int result = mapper.updateDepartment(new Department(60, "行政部22", "南京222"));
+        if (result > 0) {
+            System.out.println("更新部门操作成功");
+        }
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void deleteDepartmentTest(){
+        //获取sqlSession对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        //获取Mapper对象
+        DepartmentMapper mapper = sqlSession.getMapper(DepartmentMapper.class);
+
+        int result = mapper.deleteDepartment(60);
+        if (result > 0) {
+            System.out.println("删除部门操作成功");
+        }
+
+        sqlSession.close();
+    }
 }
