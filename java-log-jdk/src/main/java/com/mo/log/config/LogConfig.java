4c10e2a960f386ed5e6b55e1039bd8410e080938
moweiquan <295597613@qq.com>
2021-05-08 13:07:48 +0800

    java日志系统-配置文件缓存设计与实现

--- java-log-jdk/src/main/java/com/mo/log/config/LogConfig.java
+++ java-log-jdk/src/main/java/com/mo/log/config/LogConfig.java
@@ -1,9 +1,13 @@
 package com.mo.log.config;
 
+import com.sun.tools.corba.se.idl.StringGen;
+
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -13,19 +17,52 @@ public class LogConfig {
 
     public static final String configFile = "/Users/mo/develop/workspace/javaFrameworkTest/java-log-jdk/src/main/resources/log.properties";
 
+    /**
+     * 配置文件缓存
+     */
+    private static Map<String, Properties> propsMap = new HashMap<String, Properties>();
+
+    /**
+     * 获取配置
+     *
+     * @param key
+     * @return
+     */
+    public static String getConfig(String key) {
+        File file = new File(configFile);
+        String result = readProperties(file, key);
+        return result;
+    }
+
+    /**
+     * 读取配置文件的配置项
+     *
+     * @param file
+     * @param key
+     * @return
+     */
     public static String readProperties(File file, String key) {
 
         String result = "";
-        try (FileInputStream fis = new FileInputStream(file);
-             BufferedInputStream bis = new BufferedInputStream(fis);) {
+        //先从缓存里面读取配置文件
+        Properties prop = propsMap.get(configFile);
+        if (null == prop) {
+            System.out.println("缓存里面没有找到...");
+            try (FileInputStream fis = new FileInputStream(file);
+                 BufferedInputStream bis = new BufferedInputStream(fis);) {
 
-            Properties properties = new Properties();
-            properties.load(bis);
-            result = properties.getProperty(key, "");
-        } catch (IOException e) {
-
+                Properties properties = new Properties();
+                properties.load(bis);
+                //配置文件加入缓存
+                propsMap.put(configFile, properties);
+                result = properties.getProperty(key);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("进入缓存里面找....");
+            result = prop.getProperty(key);
         }
-
         return result;
 
     }
--- java-log-jdk/src/test/java/com/mo/log/test/LogConfigTest.java
+++ java-log-jdk/src/test/java/com/mo/log/test/LogConfigTest.java
@@ -13,7 +13,15 @@ public class LogConfigTest {
     @Test
     public void logConfigTest() {
         File file = new File(LogConfig.configFile);
-        String result = LogConfig.readProperties(file, "value");
+        String result = LogConfig.readProperties(file, "value1");
         System.out.println("读取到配置文件为" + result);
+    }
+
+    @Test
+    public void getConfigTest() {
+        String result1 = LogConfig.getConfig("value1");
+        String result2 = LogConfig.getConfig("value2");
+        System.out.println("读取到配置文件:value1为" + result1);
+        System.out.println("读取到配置文件:value2为" + result2);
     }
 }
