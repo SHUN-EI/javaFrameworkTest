f7c86b0bf14335fba8fe64846eeea05eed3fe568
moweiquan <295597613@qq.com>
2021-05-12 23:15:45 +0800

    java大数据检索系统-项目构建

---
+++ java-bigData-search/src/main/java/com/mo/BigDataSearchApplication.java
@@ -0,0 +1,15 @@
+package com.mo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by mo on 2021/5/12
+ */
+@SpringBootApplication
+public class BigDataSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigDataSearchApplication.class, args);
+    }
+}
