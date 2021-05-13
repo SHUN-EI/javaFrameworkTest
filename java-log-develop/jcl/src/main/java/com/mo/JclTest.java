9d6cf4e31d3f6da5fc6e1700a6061c296b9e6f65
moweiquan <295597613@qq.com>
2021-05-13 19:50:22 +0800

    java-日志的发展-JCL(Jakarta Commons Logging),日志接口

---
+++ java-log-develop/jcl/src/main/java/com/mo/JclTest.java
@@ -0,0 +1,16 @@
+package com.mo;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Created by mo on 2021/5/13
+ */
+public class JclTest {
+
+    private static final Log log = LogFactory.getLog(JclTest.class);
+
+    public static void main(String[] args) {
+        log.info("这是JCL的日志信息");
+    }
+}
