d9aaf26d6c37a39991a1ee80a73019c6edddd911
moweiquan <295597613@qq.com>
2021-05-13 19:22:39 +0800

    java-日志的发展-JUL(java.util.logging)

---
+++ java-log-develop/jul/src/main/java/com/mo/JulTest.java
@@ -0,0 +1,15 @@
+package com.mo;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by mo on 2021/5/13
+ */
+public class JulTest {
+
+    private static final Logger logger = Logger.getLogger(JulTest.class.getName());
+
+    public static void main(String[] args) {
+        logger.info("这是JUL的日志信息");
+    }
+}
