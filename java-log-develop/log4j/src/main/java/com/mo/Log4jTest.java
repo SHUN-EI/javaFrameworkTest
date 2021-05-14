d14bddc3485f2e3589331b7d229bbaa20326227f
moweiquan <295597613@qq.com>
2021-05-14 12:46:06 +0800

    java-日志的配置-配置log4j的日志级别

--- java-log-develop/log4j/src/main/java/com/mo/Log4jTest.java
+++ java-log-develop/log4j/src/main/java/com/mo/Log4jTest.java
@@ -10,6 +10,9 @@ public class Log4jTest {
     private static final Logger logger = Logger.getLogger(Log4jTest.class);
 
     public static void main(String[] args) {
-         logger.info("这是Log4j的日志信息");
+         logger.debug("debug:这是Log4j的日志信息");
+         logger.info("info:这是Log4j的日志信息");
+         logger.warn("warn:这是Log4j的日志信息");
+         logger.fatal("fatal:这是Log4j的日志信息");
     }
 }
