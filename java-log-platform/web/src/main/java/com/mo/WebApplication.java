bc7d8fe96a9253b4d0f62a04ae44c4dfa2f22ac7
moweiquan <295597613@qq.com>
2021-05-19 18:36:23 +0800

    java日志平台-引入nacos注册中心

--- java-log-platform/web/src/main/java/com/mo/WebApplication.java
+++ java-log-platform/web/src/main/java/com/mo/WebApplication.java
@@ -2,10 +2,12 @@ package com.mo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by mo on 2021/5/19
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class WebApplication {
 
