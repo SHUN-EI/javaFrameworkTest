4db0172b2a8bcc6a827ca1dcba8c1a604bd4dc45
moweiquan <295597613@qq.com>
2021-05-19 14:02:45 +0800

    java日志平台-引入java日志平台模块

---
+++ java-log-platform/web/src/main/java/com/mo/WebApplication.java
@@ -0,0 +1,15 @@
+package com.mo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by mo on 2021/5/19
+ */
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
