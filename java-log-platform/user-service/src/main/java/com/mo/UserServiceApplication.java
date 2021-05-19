f60fb139b269a03caecdf4759a3c6390b6ccb6f7
moweiquan <295597613@qq.com>
2021-05-19 18:48:46 +0800

    java日志平台-引入user-service子模块

---
+++ java-log-platform/user-service/src/main/java/com/mo/UserServiceApplication.java
@@ -0,0 +1,18 @@
+package com.mo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by mo on 2021/5/19
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
